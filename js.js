03910954bdd9f259b2a7c1ffb58068a06e44f6f4
Chris Warren <cwarren@kinsol.io>
2024-11-04 15:11:26 -0800

    added cursor button

--- js.js
+++ js.js
@@ -3,4 +3,10 @@ function test(scrollTo){
 	document.getElementById(scrollTo).scrollIntoView({
 		behavior: 'smooth'
 	});
-}
+};
+
+function log() {
+	console.log('Clicked');
+	document.getElementById('body_id').classList.add('cursor_class');
+	document.getElementById('sword_button').classList.add('hidden_class');
+};
